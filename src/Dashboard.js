ddac652456bd0e3d34677e4339471e4e04ab2c7e
ajkatom <alonadelson@alons-MacBook-Pro.local>
2020-04-14 14:52:14 -0400

    bug fix

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -12,11 +12,7 @@ class Dashboard extends Component {
     super();
     this.state = {
       showForm: false,
-<<<<<<< HEAD
       productId: null,
-=======
-      productId: null
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
     };
     this.onClick = this.onClick.bind(this);
     this.onChange = this.onChange.bind(this);
@@ -53,39 +49,21 @@ class Dashboard extends Component {
   }
 
   render() {
-<<<<<<< HEAD
     const { orders, categories, products, user, users } = this.props;
-=======
-    const {
-      orders,
-      categories,
-      products,
-      user,
-      users
-    } = this.props;
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
     const { onChange, onClick, hide, makeAdmin } = this;
     const { showForm, productId } = this.state;
 
     return !user.admin ? null : (
-<<<<<<< HEAD
       <div className="dashboard">
         <div className="dashboard-item">
           <h1>Users</h1>
           <div className="dashboard-users">
-=======
-      <div className='dashboard'>
-        <div className='dashboard-item'>
-          <h1>Users</h1>
-          <div className='dashboard-users'>
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
             <h5>Name</h5>
             <h5>Email</h5>
             <h5>Admin?</h5>
             {users &&
               users.map(_user => {
                 return (
-<<<<<<< HEAD
                   <div className="user-row" key={_user.id}>
                     <strong>{_user.name}</strong>
                     <Link to={`mailto:${_user.email}`}>{_user.email}</Link>
@@ -98,20 +76,6 @@ class Dashboard extends Component {
                           onChange={() => makeAdmin(_user)}
                         />
                         <span className="slider" />
-=======
-                  <div className='user-row' key={_user.id}>
-                    <strong>{_user.name}</strong>
-                    <Link to={`mailto:${_user.email}`}>{_user.email}</Link>
-                    <div>
-                      <label className='switch'>
-                        <input
-                          checked={_user.admin}
-                          type='checkbox'
-                          datatype='toggle'
-                          onChange={() => makeAdmin(_user)}
-                        />
-                        <span className='slider' />
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
                       </label>
                     </div>
                   </div>
@@ -119,20 +83,13 @@ class Dashboard extends Component {
               })}
           </div>
         </div>
-<<<<<<< HEAD
         <div className="dashboard-item">
           <h1>All Orders</h1>
           <div className="dashboard-orders">
-=======
-        <div className='dashboard-item'>
-          <h1>All Orders</h1>
-          <div className='dashboard-orders'>
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
             <h5>Order ID</h5>
             <h5>Shipped?</h5>
             {orders.length &&
               orders.map(order => (
-<<<<<<< HEAD
                 <div className="dashboard-order-row" key={order.id}>
                   <Link to={`/orders/${order.id}`}> {order.id}</Link>
                   <div>
@@ -144,51 +101,25 @@ class Dashboard extends Component {
                         onChange={() => onChange(order)}
                       />
                       <span className="slider" />
-=======
-                <div className='dashboard-order-row' key={order.id}>
-                  <Link to={`/orders/${order.id}`}> {order.id}</Link>
-                  <div>
-                    <label className='switch'>
-                      <input
-                        checked={order.shipped}
-                        type='checkbox'
-                        datatype='toggle'
-                        onChange={() => onChange(order)}
-                      />
-                      <span className='slider' />
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
                     </label>
                   </div>
                 </div>
               ))}
           </div>
         </div>
-<<<<<<< HEAD
         <div className="dashboard-item">
-=======
-        <div className='dashboard-item'>
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
           <h1>Products By Category</h1>
           {categories.map(category => {
             return (
               <div key={category.id}>
                 <h2>{category.name}</h2>
-<<<<<<< HEAD
                 <div className="btn-group">
-=======
-                <div className='btn-group'>
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
                   {products.map(product => {
                     if (product.categoryId === category.id) {
                       return (
                         <button
-<<<<<<< HEAD
                           type="submit"
                           className="btn btn-secondary"
-=======
-                          type='submit'
-                          className='btn btn-secondary'
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
                           key={product.id}
                           onClick={() => onClick(product.id)}
                         >
@@ -196,10 +127,6 @@ class Dashboard extends Component {
                         </button>
                       );
                     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
                   })}
                 </div>
               </div>
@@ -210,13 +137,9 @@ class Dashboard extends Component {
           {showForm && <ProductForm productId={productId} hide={hide} />}
           <br />
           <div>
-<<<<<<< HEAD
             <button type="submit" className="btn btn-secondary" onClick={() => onClick(null)}>
               Add New Product
             </button>
-=======
-            <button type='submit' className='btn btn-secondary' onClick={() => onClick(null)}>Add New Product</button>
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
           </div>
         </div>
       </div>
@@ -230,11 +153,7 @@ const mapStateToProps = ({ user, orders, products, categories, users }) => {
     orders,
     categories,
     products,
-<<<<<<< HEAD
     users,
-=======
-    users
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
   };
 };
 
@@ -248,11 +167,7 @@ const mapDispatchToProps = dispatch => {
     },
     createOrUpdateUser: user => {
       dispatch(createOrUpdateUser(user, null, true));
-<<<<<<< HEAD
     },
-=======
-    }
->>>>>>> 30cda0fc42ea8eee22e8b00f0af07ab93634c89b
   };
 };
 
